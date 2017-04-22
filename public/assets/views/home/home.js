a16013e8cc3103928799c86cb52043fcb6a5a74a
max lykov <frise.on@gmail.com>
2017-04-22 22:23:53 +0500

    redisgn shedule

--- public/assets/views/home/home.js
+++ public/assets/views/home/home.js
@@ -6,16 +6,20 @@
         .controller('homeCtrl', homeCtrl);
 
         homeCtrl.$inject = [
-            '$scope', 'appService', '$uibModal', 'modalsConstants'
+            '$scope', 'appService', '$uibModal', 'modalsConstants', 'scheduleConstants'
         ];
 
-        function homeCtrl($scope, appService, $uibModal, modalsConstants) {
+        function homeCtrl($scope, appService, $uibModal, modalsConstants, scheduleConstants) {
         	var model = this;
 
             model.schedule = [];
+            model.months = [];
 
             var getSchedule = function() {
                 appService.getSchedule().then(function(data) {
+                    for (var key in data) {
+                        model.months.push(scheduleConstants.months[key.split(".")[0]] + " " + key.split(".")[1]);
+                    }
                     console.log(data);
                     model.schedule = data;
                 });
